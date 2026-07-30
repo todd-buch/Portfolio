8c8305f69f87db280a045257dfc1171eadc52585
Todd Buch <todd.buch@outlook.com>
2026-07-30 15:22:38 -0400

    Name & traits

---
+++ src/Main_Projects.tsx
@@ -0,0 +1,16 @@
+import { Dot } from "lucide-react";
+
+export default function Main_Projects() {
+  return (
+    <>
+      <div className="projects">
+        <div className="bio">
+          <h1>Todd Buch</h1>
+          <p>
+          Creative <Dot /> Observant <Dot /> Curious <Dot /> Analytical <Dot /> Solution-Oriented
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
