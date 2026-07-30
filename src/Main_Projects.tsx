4c9130126237482d1fa919d3b7b6c01dd590800e
Todd Buch <todd.buch@outlook.com>
2026-07-30 15:35:59 -0400

    Initial bio.

--- src/Main_Projects.tsx
+++ src/Main_Projects.tsx
@@ -6,9 +6,19 @@ export default function Main_Projects() {
       <div className="projects">
         <div className="bio">
           <h1>Todd Buch</h1>
-          <p>
-          Creative <Dot /> Observant <Dot /> Curious <Dot /> Analytical <Dot /> Solution-Oriented
-          </p>
+          <div className="projects-traits">
+            <p>Creative</p>
+            <Dot />
+            <p>Observant</p>
+            <Dot />
+            <p>Curious</p>
+            <Dot />
+            <p>Analytical</p>
+            <Dot />
+            <p>Solution-Oriented</p>
+            <Dot />
+          </div>
+          <p>I'm a computer science student studying software development & design. I love digital media including photography and graphic design.</p>
         </div>
       </div>
     </>
