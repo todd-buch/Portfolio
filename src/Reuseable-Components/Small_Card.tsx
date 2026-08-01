6e82836d48f49fe3a0f0b80c679aa2b807966dde
Todd Buch <todd.buch@outlook.com>
2026-08-01 16:02:44 -0400

    home-projects: get in touch section

--- src/Reuseable-Components/Small_Card.tsx
+++ src/Reuseable-Components/Small_Card.tsx
@@ -3,14 +3,18 @@ import "./Small_Card.css";
 
 interface SmallCardProps {
   title: string;
+  date:string;
   children: ReactNode;
 }
 
-export default function Small_Card({ title, children }: SmallCardProps) {
+export default function Small_Card({ title, date, children }: SmallCardProps) {
   return (
     <>
       <div className="small-card">
-        <p className="small-card-title">{title}</p>
+        <div className="small-card-top">
+          <p className="small-card-title">{title}</p>
+          <p className="small-card-date">{date}</p>
+        </div>
         <div className="small-card-content">{children}</div>
       </div>
     </>
